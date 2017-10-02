3251d2f55a51dfac2cbf8326d672447d6c4d5887
Luiz Gustavo Stábile de Souza <luizgustavoss@gmail.com>
2017-10-02 18:03:43 -0300

    Ajustes para inclusão de filtros e parametrizações

---
+++ ogc-services/wmts/openlayers/map-definition.js
@@ -0,0 +1,54 @@
+var projection = ol.proj.get('EPSG:3857');
+var projectionExtent = projection.getExtent();
+var size = ol.extent.getWidth(projectionExtent) / 256;
+var resolutions = new Array(14);
+var matrixIds = new Array(14);
+
+for (var z = 0; z < 14; ++z) {
+    // generate resolutions and matrixIds arrays for this WMTS
+    resolutions[z] = size / Math.pow(2, z);
+    matrixIds[z] = 'EPSG:3857:' + z;
+}
+
+
+function defineMap(requestParam, ) {
+
+    //if (config) {
+    var map = new ol.Map({
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+                opacity: 1
+            }),
+            new ol.layer.Tile({
+                opacity: 1,
+                source: new ol.source.WMTS({
+                    url: 'https://services.digitalglobe.com/earthservice/wmtsaccess?connectid=3b31d310-97e7-434d-8e9c-632e007b753a&featureProfile=Accuracy_Profile',
+                    layer: 'DigitalGlobe:ImageryTileService',
+                    matrixSet: 'EPSG:3857',
+                    format: 'image/jpeg',
+                    projection: projection,
+                    tileGrid: new ol.tilegrid.WMTS({
+                        origin: ol.extent.getTopLeft(projectionExtent),
+                        resolutions: resolutions,
+                        matrixIds: matrixIds
+                    }),
+                    style: '',
+                    wrapX: true
+                })
+            })
+        ],
+        target: 'map',
+        controls: ol.control.defaults({
+            attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+                collapsible: false
+            })
+        }),
+        view: new ol.View({
+            center: [-11158582, 4813697],
+            zoom: 4
+        })
+    });
+    //}
+
+}
